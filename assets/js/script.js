fc558fee8f8174dc8e8e0ce7242b3ea16e86e7a8
Konstantin Pak <konstantin.pak.it@gmail.com>
2023-04-14 13:05:16 +0600

    Валидация формы завершена

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,12 @@ const overlay = document.querySelector(".overlay");
 const faqItems = document.querySelectorAll(".faq-list__item");
 const body = document.querySelector("body");
 
+
+
+
+
+
+
 mobileMenu.addEventListener("click", function() {
   toggleFunc(accordion);
   toggleFunc(mobileMenu);
@@ -26,3 +32,94 @@ faqItems.forEach(o => {
     toggleFunc(o);
   });
 });
+
+
+const form = document.querySelector("form");
+const last_name = document.getElementById("lname");
+const first_name = document.getElementById("fname");
+const country = document.getElementById("country");
+const tel = document.getElementById("tel");
+const email = document.getElementById("email");
+const textarea = document.getElementById("textarea");
+const inputControl = document.querySelector(".form__input-wrapper");
+
+
+
+// Event Listener of the form
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validateInputs();
+}); 
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add("_error");
+  inputControl.classList.remove("_success");
+}
+
+const setSuccess = element => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = '';
+  inputControl.classList.add('_success');
+  inputControl.classList.remove('_error');
+}
+
+const isValidEmail = email => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+
+}
+
+const validateInputs = () => {
+  const lastNameValue = last_name.value.trim();
+  const firstNameValue = first_name.value.trim();
+  const countryValue = country.value.trim();
+  const telValue = tel.value.trim();
+  const emailValue = email.value.trim();
+  const textareaValue = textarea.value.trim();
+
+  if(lastNameValue === '') {
+    setError(last_name, 'Введите вашу фамилию');
+  } else {
+    setSuccess(last_name);
+  }
+
+  
+  if(firstNameValue === '') {
+    setError(first_name, 'Введите ваше ваше имя');
+  } else {
+    setSuccess(first_name);
+  }
+
+  if(countryValue === '') {
+    setError(country, 'Введите вашу страну');
+  } else {
+    setSuccess(country);
+  }
+
+  if(telValue === '') {
+    setError(tel, 'Введите ваш номер телефона');
+  } else {
+    setSuccess(tel);
+  }
+
+  if(emailValue === '') {
+    setError(email, 'Введите ваш email');
+  } else if (!isValidEmail(emailValue)) {
+    setError(email, 'Введите верный формат');
+  } else {
+    setSuccess(email);
+  }
+
+  if(textareaValue === '') {
+    setError(textarea, 'Задайте ваш вопрос');
+  } else {
+    setSuccess(textarea);
+  }
+
+}
